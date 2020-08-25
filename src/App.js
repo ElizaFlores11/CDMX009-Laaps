855fb7ad07d410aaf1835933e4d12791b8505e99
abigail-sanlo <abisl9160@gmail.com>
2020-08-25 16:26:33 -0500

    start abi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {Fragment} from 'react';
 import Routes from './componets/Routes'; 
 import Register from './componets/Form';
+import NavDown from './componets/Navbar';
 import './App.scss';
 function App() {
   return (
     <Fragment>
       <Routes />
       <Register/>
-     
+      <NavDown/>
     </Fragment>
+
   );
 }
 
---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
+
+const NavDown = ()=>{
+    return (
+        <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Navbar>
+    )
+}
+;
+export default NavDown;
