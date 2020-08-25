c247f35c7282c834acb1b5635b953c3e3f238906
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-08-25 09:03:57 -0500

    componwnts

--- src/componets/Logo/Logo.js
+++ src/componets/Logo/Logo.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import logo1st from '../../assets/logo1st.png'
 import logo2nd from '../../assets/logo2nd.png'
-const Logo = ({small= false, big= false}) =>{
+const Logo = ({small= false, big= false, medium=false }) =>{
     let src = logo1st
     let alt = logo1st
+    let style = {
+        width: '70%',
+        height: '70%'
+    }
+    
 
     if(big){
         src = logo1st
@@ -11,9 +16,15 @@ const Logo = ({small= false, big= false}) =>{
     } else if (small){
         src = logo2nd
         alt = logo2nd
+    } else if (medium){
+        style = {
+            width: '30%',
+            height: '30%'
+        }
+        alt = 'medium'
     }
     return(
-        <img src={src} alt={alt}></img>
+        <img src={src} alt={alt} style={style}></img>
     )
 }
 
