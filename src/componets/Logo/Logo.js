2c21098631f258ff7edb904db276d1839b08fbc2
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-08-24 23:14:34 -0500

    component logo ready

--- src/componets/Logo/Logo.js
+++ src/componets/Logo/Logo.js
@@ -1,1 +1,20 @@
 import React from 'react'
+import logo1st from '../../assets/logo1st.png'
+import logo2nd from '../../assets/logo2nd.png'
+const Logo = ({small= false, big= false}) =>{
+    let src = logo1st
+    let alt = logo1st
+
+    if(big){
+        src = logo1st
+        alt = logo1st
+    } else if (small){
+        src = logo2nd
+        alt = logo2nd
+    }
+    return(
+        <img src={src} alt={alt}></img>
+    )
+}
+
+export default Logo
