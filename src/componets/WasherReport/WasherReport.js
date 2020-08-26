6411386ba6d7a5ac032d92824cbfa2e4aaaaa6da
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-08-26 00:56:56 -0500

    Washer Repor Admin

--- src/componets/WasherReport/WasherReport.js
+++ src/componets/WasherReport/WasherReport.js
@@ -10,6 +10,59 @@ const  WasherReport = () =>{
             <HeaderAdmin />
             <p className="titulo">Lavadores</p>
             <p className="titulo2">Registrados</p>
+            <div className='washer-container'>
+            <div className='icon'>
+            <i class="fas fa-user fa-3x"></i>
+            </div>
+            <div className='card'>
+                 <div className="titulo2">Abigail Sanches</div>
+            </div>
+            <div className='calf'>
+            <i className="fas fa-star mas"></i>
+            MÁS
+            <i class="fas fa-chevron-right"></i>
+            </div>
+            </div>
+            <div className='washer-container'>
+            <div className='icon'>
+            <i class="fas fa-user fa-3x"></i>
+            </div>
+            <div className='card'>
+                 <div className="titulo2">Karen Flores</div>
+            </div>
+            <div className='calf'>
+            <i className="fas fa-star mas"></i>
+            MÁS
+            <i class="fas fa-chevron-right"></i>
+            </div>
+            </div>
+            <div className='washer-container'>
+            <div className='icon'>
+            <i class="fas fa-user fa-3x"></i>
+            </div>
+            <div className='card'>
+                 <div className="titulo2">Mariel </div>
+            </div>
+            <div className='calf'>
+            <i className="fas fa-star mas"></i>
+            MÁS
+            <i class="fas fa-chevron-right"></i>
+            </div>
+            </div>
+            <div className='washer-container'>
+            <div className='icon'>
+            <i class="fas fa-user fa-3x"></i>
+            </div>
+            <div className='card'>
+                 <div className="titulo2">Elizabeth Flores</div>
+            </div>
+            <div className='calf'>
+            <i className="fas fa-star mas"></i>
+            MÁS
+            <i class="fas fa-chevron-right"></i>
+            </div>
+            </div>
+        
         </Fragment>
     )
 }
